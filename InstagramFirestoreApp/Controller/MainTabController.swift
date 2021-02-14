3320c533cfdccc96c4610613c5f34834ae327c5f
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-02-14 18:27:47 +0100

    Initial Setup. This project doesnt use Storyboard

---
+++ InstagramFirestoreApp/Controller/MainTabController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreApp
+//
+//  Created by Tobiloba Owolabi on 14/02/2021.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController{
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
