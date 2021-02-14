2a17207eceaa6db607e828a4c3a3a0258d158ccc
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-02-14 17:42:25 +0100

    Initial Commit

---
+++ InstagramFirestoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestoreApp
+//
+//  Created by Tobiloba Owolabi on 14/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
